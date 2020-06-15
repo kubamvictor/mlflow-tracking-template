0c60e0da3861c1eea97677fc3c719592d3a1632a
Victor Kubam <kubamvictor@gmail.com>
2020-06-15 12:15:33 +0200

    update: document code

--- tracking.py
+++ tracking.py
@@ -1,9 +1,12 @@
 import mlflow
 
-# Configure mlflow
+# Configure mlflow: Location to save all mlflow related data
 remote_server_uri = "/Users/Kubam/projects/machine_learning/data/mlflow"
 
+# Pointing mlflow to the where it has to save its data
 mlflow.set_tracking_uri("file:"+remote_server_uri)
+
+# Experiment name
 mlflow.set_experiment("/experiment-name")
 
 # Start run
@@ -18,5 +21,5 @@ mlflow.log_metric("acc", 0.99)
 mlflow.end_run()
 
 
-# See ui
+# MLflow UI
 # mlflow server - -backend-store-uri < remote_server_uri > - -default-artifact-root < remote_server_uri > - -host 0.0.0.0: 5000
