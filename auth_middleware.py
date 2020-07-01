2b387bd4af59f6a91ab1ab385d9c67c22a5ac24b
Victor Kubam <kubamvictor@gmail.com>
2020-07-01 20:30:44 +0200

    mlflow bearer authentication added

--- auth_middleware.py
+++ auth_middleware.py
@@ -1,11 +1,42 @@
-class AuthMiddleware(object):
+class AuthMiddleware():
+    """
+    Middleware for authenticating requests sent to a flask app
+    """
 
     def __init__(self, app):
         self.app = app
+        self.token = 'YHON36336hhngEYEY'
 
     def __call__(self, environ, start_response):
-        print('----------------------------')
-        print('Middleware function called')
-        print('----------------------------')
+        # Validate token
+        if self._authenticated(environ.get('HTTP_AUTHORIZATION')):
+            return self.app(environ, start_response)
+        return self._login(environ, start_response)
 
-        return self.app(environ, start_response)
+    def _authenticated(self, header):
+        """
+        Function to extract and validate user token from request header
+
+        argument:
+            header: request header
+
+        output: 
+            boolean: if user is validated this is set to true, otherwise false
+        """
+
+        try:
+            if not header:
+                return False
+
+            _, token = header.split(None, 1)
+            print("User has been validated? {}".format(token == self.token))
+
+            return token == self.token
+        except Exception:
+            return False
+
+    def _login(self, environ, start_response):
+        start_response(
+            '401 Authentication Required',
+            [('Content-Type', 'text/html'), ('WWW-Authenticate', 'Bearer realm="mlflow server"')])
+        return [b'Authentication Failed! Please check your authorization token or contact system admins.']
--- tracking_api_example.py
+++ tracking_api_example.py
@@ -1,24 +1,39 @@
 import mlflow
+import os
+
+os.environ['MLFLOW_TRACKING_TOKEN'] = 'YHON36336hhngEYEY'
 
 # Configure mlflow: Location to save all mlflow related data
-remote_server_uri = "/Users/Kubam/projects/machine_learning/data/mlflow"
+remote_server_uri = "http://localhost:8000"
 
-# Pointing mlflow to the where it has to save its data
-mlflow.set_tracking_uri("file:"+remote_server_uri)
+if 'http' in remote_server_uri:
+    remote_server_uri = remote_server_uri
+else:
+    remote_server_uri = 'file:'+remote_server_uri
 
-# Experiment name
-mlflow.set_experiment("/experiment-name")
+try:
+    # Pointing mlflow to the where it has to save its data
+    mlflow.set_tracking_uri(remote_server_uri)
+    # Experiment name
+    mlflow.set_experiment("/experiment-name")
+    mlflow_tracking = True
+except Exception:
+    mlflow_tracking = False
+
 
 # Start run
-mlflow.start_run()
+if mlflow_tracking:
+    mlflow.start_run()
 
 
 # Start tracking
-mlflow.log_param("hello", "World")
-mlflow.log_metric("acc", 0.99)
+if mlflow_tracking:
+    mlflow.log_param("hello", "World")
+    mlflow.log_metric("acc", 0.99)
 
 # End run
-mlflow.end_run()
+if mlflow_tracking:
+    mlflow.end_run()
 
 
 # MLflow UI
